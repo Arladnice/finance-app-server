4ba93309d2d74a16844c14a9981018769fe6ace1
Denis Arlakov <darlakov@mail.ru>
2023-05-02 13:52:08 +0300

    cors

--- index.js
+++ index.js
@@ -22,7 +22,9 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000", "https://render.com/"]
+}));
 
 // ROUTES
 app.use("/kpi", kpiRoutes);
